b877582277edf7ea37ce36306d9276ad3d53011c
ia5go <benvindoiago@outlook.com>
2023-09-06 20:20:19 -0300

    conteúdo da index e um pouco de sass

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,14 @@
 new Splide(document.querySelector(".banner"), {
+  type: "loop",
+  autoWidth: true,
+  autoHeight: true,
+  autoplay: true,
+  pagination: false,
+  arrows: false,
+}).mount();
+
+new Splide(document.querySelector(".stack__carousel"), {
+  type: "loop",
   autoWidth: true,
   autoHeight: true,
   autoplay: true,

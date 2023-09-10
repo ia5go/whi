6c99fac9ab9aed3074a5bdd7b7668880dfb8bd7e
ia5go <benvindoiago@outlook.com>
2023-09-10 20:51:02 -0300

    um pouco de estilo e front no tempinho que tive

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,9 +9,11 @@ new Splide(document.querySelector(".banner"), {
 
 new Splide(document.querySelector(".stack__carousel"), {
   type: "loop",
-  autoWidth: true,
-  autoHeight: true,
-  autoplay: true,
-  pagination: false,
+  perPage: 4,
+  parMove: 4,
+  autoplay: false,
+  pagination: true,
   arrows: false,
+  speed: 1800,
+  easing: "cubic-bezier(0.5, .6, 0.5, 1)",
 }).mount();

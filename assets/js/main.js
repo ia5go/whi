e381e081fda480aa0165dcf0a5583906e2ab6694
ia5go <benvindoiago@outlook.com>
2023-11-02 18:20:15 -0300

    Nava área  de projetos

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,11 +10,11 @@ new Splide(document.querySelector(".mdl-banner"), {
 new Splide(document.querySelector(".mdl-stack__slider"), {
   perPage: 1,
   parMove: 1,
+  autoHeight: true,
+  autoWidth: true,
   focus: "center",
   pagination: false,
   arrows: false,
-  autoHeight: true,
-  autoWidth: true,
   speed: 1000,
   easing: "cubic-bezier(0.5, .6, 0.5, 1)",
 
@@ -24,3 +24,41 @@ new Splide(document.querySelector(".mdl-stack__slider"), {
     },
   },
 }).mount();
+
+const projectsSlider = new Splide(document.querySelector(".mdl-projects__slider"), {
+  perPage: 3,
+  perMove: 3,
+  focus: "center",
+  pagination: false,
+  arrows: false,
+  easing: "cubic-bezier(0.5, .6, 0.5, 1)",
+
+  breakpoints: {
+    990: {
+      perPage: 2,
+      perMove: 2,
+    },
+    520: {
+      perPage: 1,
+      perMove: 1,
+    },
+  },
+});
+
+const projectsSliderNav = new Splide(document.querySelector(".mdl-projects__slider-nav"), {
+  perPage: 1,
+  perMove: 1,
+  pagination: false,
+  arrows: false,
+  rewind: true,
+  gap: 10,
+  autoHeight: true,
+  autoWidth: true,
+  autoplay: true,
+  focus: "center",
+  isNavigation: true,
+});
+
+projectsSlider.sync(projectsSliderNav);
+projectsSlider.mount();
+projectsSliderNav.mount();

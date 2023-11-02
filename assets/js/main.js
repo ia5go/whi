2b95ce07d3233b3684c9f9662193e27c5424a5c8
ia5go <benvindoiago@outlook.com>
2023-11-02 15:22:26 -0300

    ajustes pedidos pela whi

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,32 +8,19 @@ new Splide(document.querySelector(".mdl-banner"), {
 }).mount();
 
 new Splide(document.querySelector(".mdl-stack__slider"), {
-  type: "loop",
-  perPage: 8,
+  perPage: 1,
   parMove: 1,
-  gap: 52,
+  focus: "center",
   pagination: false,
   arrows: false,
-  autoplay: true,
+  autoHeight: true,
+  autoWidth: true,
   speed: 1000,
   easing: "cubic-bezier(0.5, .6, 0.5, 1)",
 
   breakpoints: {
     1080: {
-      perPage: 7,
-    },
-    920: {
-      perPage: 6,
-    },
-    760: {
-      perPage: 5,
-    },
-    620: {
-      perPage: 4,
-      gap: 36,
-    },
-    500: {
-      perPage: 3,
+      autoplay: true,
     },
   },
 }).mount();
